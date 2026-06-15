2bf723a7d606ad6368c81ffce7e169c064070809
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2026-06-15 17:54:11 +0200

    Rename build outputs to jfrog_sbom_package_checker

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Windows
-GOOS=windows GOARCH=amd64 go build -o build/check_packages_windows_amd64.exe check_packages.go
-GOOS=windows GOARCH=386   go build -o build/check_packages_windows_386.exe check_packages.go
-GOOS=windows GOARCH=arm64 go build -o build/check_packages_windows_arm64.exe check_packages.go
+GOOS=windows GOARCH=amd64 go build -o build/jfrog_sbom_package_checker_windows_amd64.exe jfrog_sbom_package_checker.go
+GOOS=windows GOARCH=386   go build -o build/jfrog_sbom_package_checker_windows_386.exe jfrog_sbom_package_checker.go
+GOOS=windows GOARCH=arm64 go build -o build/jfrog_sbom_package_checker_windows_arm64.exe jfrog_sbom_package_checker.go
 
 # Linux
-GOOS=linux GOARCH=amd64 go build -o build/check_packages_linux_amd64 check_packages.go
-GOOS=linux GOARCH=arm64 go build -o build/check_packages_linux_arm64 check_packages.go
+GOOS=linux GOARCH=amd64 go build -o build/jfrog_sbom_package_checker_linux_amd64 jfrog_sbom_package_checker.go
+GOOS=linux GOARCH=arm64 go build -o build/jfrog_sbom_package_checker_linux_arm64 jfrog_sbom_package_checker.go
 
 # macOS
-GOOS=darwin GOARCH=amd64 go build -o build/check_packages_macos_amd64 check_packages.go
-GOOS=darwin GOARCH=arm64 go build -o build/check_packages_macos_arm64 check_packages.go
+GOOS=darwin GOARCH=amd64 go build -o build/jfrog_sbom_package_checker_macos_amd64 jfrog_sbom_package_checker.go
+GOOS=darwin GOARCH=arm64 go build -o build/jfrog_sbom_package_checker_macos_arm64 jfrog_sbom_package_checker.go
 
 echo "✅ All builds complete in ./build/"
